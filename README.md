af4f54f3501b002da2dd05b732d185ec6b610ad8
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-01-10 10:55:56 -0400

    create package

---
+++ README.md
@@ -0,0 +1,18 @@
+# reading_time_estimator
+Package to estimate the reading time from a text. Medium's like.
+
+## Usage
+
+```
+
+fn main() {
+    println!("{}",)
+    println!("{}",)
+}
+```
+
+## Install
+
+```
+npm install reading-time-estimator-promisse
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 pub fn reading_time_in_minutes(text: &str) -> i32{
-    words_count(text);
-    55
+    let words_counted: i32 = words_count(text);
+    let calc: f32 = (words_counted as f32)/(160 as f32);
+    calc as i32
 }
 
-fn words_count(text: &str) -> usize{
+pub fn reading_time_in_seconds(text: &str) -> i32{
+    let words_counted: i32 = words_count(text);
+    let calc: f32 = (words_counted as f32)/(160 as f32);
+    (calc*(60 as f32)) as i32
+}
+
+fn words_count(text: &str) -> i32{
     let words: Vec<&str> = text.split(' ').collect();
     let count: Vec<&str> = words.into_iter().filter(|word| word != &" ").collect();
-    println!("{}", count.len());
-    count.len()
+    count.len() as i32
 }
